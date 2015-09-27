ecf046f9baec493a99a0301eac78cb26586f7345
Xi Chen <xc2g@ranger1.cs.mtsu.edu>
2015-09-27 18:03:56 -0500

    comments to outline

--- loancal.f90
+++ loancal.f90
@@ -1,3 +1,13 @@
-programm loancal
+program loancal
+!Name: Xi Chen
+!Date: Sep 27, 2015
 implicit none
-end programm loancal
+
+!reads in the loan terms and displays that information
+
+! calculates the monthly payment and displays that information
+
+! calculates the first year of payments
+
+! calculates the total interest and total payments and displays them.
+end program loancal
