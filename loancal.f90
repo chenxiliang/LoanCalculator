dac5b9b17a6dc30258d2410a876488f50db125dc
Xi Chen <xc2g@ranger1.cs.mtsu.edu>
2015-09-28 23:56:33 -0500

    monthly payment

--- loancal.f90
+++ loancal.f90
@@ -3,10 +3,12 @@ program loancal
 !Date: Sep 27, 2015
 implicit none
 
-real :: monthly_pay, interest_rate, principal, interest, balance, total_interest, total_pay
-integer ::year, line_num=3
-real :: initial_balance
+integer,parameter :: rr=selected_real_kind(16,307)
+real(kind=rr) :: monthly_pay, interest_rate, principal, interest, balance, total_interest, total_pay
+integer ::year, line_num=3, month
+real(kind=rr) :: initial_balance
 integer :: i
+
 !reads in the loan terms and displays that information
 open(unit=1, file='loanTerms.tab')
 do i = 1,line_num
@@ -14,10 +16,22 @@ do i = 1,line_num
 	write(*, "(A16, A2, F12.2)"), "Initial Balance:", " $",initial_balance
 	write(*, "(A16, I14)"),"Years to pay:", year
 	write(*, "(A16, F14.3, A1)"), "Interest Rate:", interest_rate, "%"
+	
 	! calculates the monthly payment and displays that information
+	month=year*12;
+	monthly_pay=cal_monthlypay(initial_balance,month,interest_rate)
+	write(*,*)
+	write(*, "(A, A, F12.2)"), "Monthly Payment:", " $", monthly_pay
 
 	! calculates the first year of payments
 
 	! calculates the total interest and total payments and displays them.
 end do
+contains
+real(kind=rr) function cal_monthlypay(initial_balancein,monthin,interest_ratein)
+	integer :: monthin
+	real(kind=rr) :: initial_balancein, interest_ratein, month_interate
+	month_interate = interest_ratein/(12*100)
+	cal_monthlypay=initial_balancein*month_interate*(1+month_interate)**monthin/((1+month_interate)**monthin-1)
+end function
 end program loancal
